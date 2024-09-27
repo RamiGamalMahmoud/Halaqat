4839b30c6669b28e0dbab43ba5dd1539f1ad88d2
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2024-09-27 15:16:40 +0300

    Create CommandHandlers.cs

---
+++ App/Halaqat.Data/CommandHandlers.cs
@@ -0,0 +1,21 @@
+﻿using Halaqat.Shared.Commands;
+using Halaqat.Shared.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Halaqat.Data
+{
+    internal class GetAllGendersCommandHandlers(IAppDbContextFactory dbContextFactory) : IRequestHandler<Shared.Commands.Common.GetAllCommand<Gender>, IEnumerable<Gender>>
+    {
+        public async Task<IEnumerable<Gender>> Handle(Common.GetAllCommand<Gender> request, CancellationToken cancellationToken)
+        {
+            using (AppDbContext dbContext = dbContextFactory.CreateAppDbContext())
+            {
+                return await dbContext.Set<Gender>().ToListAsync();
+            }
+        }
+    }
+}
