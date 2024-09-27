f2a047d903481748c10697f53139f60d9adab78b
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2024-09-27 15:16:53 +0300

    Create ServiceCollectionExtension.cs

---
+++ App/Halaqat.Data/ServiceCollectionExtension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Halaqat.Data
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection ConfigureData(this IServiceCollection services)
+        {
+            services.AddMediatR((cfg) => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+            return services;
+        }
+    }
+}
