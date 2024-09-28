ee4b06914ee504c9f258207897eaa546ffc45752
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2024-09-28 21:14:11 +0300

    add GetTeacherCommand and CommandHandle

---
+++ App/Halaqat.Features.Employees/CommandHandlers/GetTeachersCommandHandlers.cs
@@ -0,0 +1,16 @@
+﻿using Halaqat.Shared.Models;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Halaqat.Features.Employees.CommandHandlers
+{
+    internal class GetTeachersCommandHandlers(Repository repository) : IRequestHandler<Shared.Commands.Employees.GetTeachersCommand, IEnumerable<Employee>>
+    {
+        public Task<IEnumerable<Employee>> Handle(Shared.Commands.Employees.GetTeachersCommand request, CancellationToken cancellationToken)
+        {
+            return repository.GetTeachers();
+        }
+    }
+}
--- App/Halaqat.Shared/Commands/Employees.cs
+++ App/Halaqat.Shared/Commands/Employees.cs
@@ -7,5 +7,6 @@ namespace Halaqat.Shared.Commands
     public static class Employees
     {
         public record GetByName(string Name) : IRequest<IEnumerable<Employee>>;
+        public record GetTeachersCommand() : IRequest<IEnumerable<Employee>>;
     }
 }
