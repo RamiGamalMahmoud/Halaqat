711843ff78040f4004330278b958dcd6a0340c5b
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2024-09-28 12:05:34 +0300

    Create CircleConfiguration.cs

---
+++ App/Halaqat.Data/Configurations/CircleConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Halaqat.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Halaqat.Data.Configurations
+{
+    internal class CircleConfiguration : IEntityTypeConfiguration<Circle>
+    {
+        public void Configure(EntityTypeBuilder<Circle> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).HasColumnType("varchar(50)").IsRequired();
+        }
+    }
+}
