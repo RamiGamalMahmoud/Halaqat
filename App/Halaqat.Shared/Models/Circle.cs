357873bf3e5ba7d192bd02d5d2cc7b1648d67be8
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2024-09-28 12:04:01 +0300

    Create Circle.cs

---
+++ App/Halaqat.Shared/Models/Circle.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+
+namespace Halaqat.Shared.Models
+{
+    [ObservableObject]
+    public partial class Circle : DeletableObject
+    {
+        private Circle()
+        {
+
+        }
+
+        public Circle(string name)
+        {
+            Name = name;
+        }
+        public DateTime DateCreated { get => _dateCreated; private set => SetProperty(ref _dateCreated, value); }
+        private DateTime _dateCreated;
+
+        [ObservableProperty]
+        private string _name;
+    }
+}
