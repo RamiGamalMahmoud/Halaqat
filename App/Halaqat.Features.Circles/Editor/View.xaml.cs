f9ca7629a7ded5f64062ed20ed9dac31a5b2509b
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2024-09-28 18:04:49 +0300

    add IMessenger as a constructor parameter to View

--- App/Halaqat.Features.Circles/Editor/View.xaml.cs
+++ App/Halaqat.Features.Circles/Editor/View.xaml.cs
@@ -1,13 +1,18 @@
-﻿using System.Windows;
+﻿using CommunityToolkit.Mvvm.Messaging;
+using Halaqat.Shared.Models;
+using System.Windows;
 
 namespace Halaqat.Features.Circles.Editor
 {
     internal partial class View : Window
     {
-        public View(ViewModel viewModel)
+        public View(ViewModel viewModel, IMessenger messenger)
         {
             InitializeComponent();
             DataContext = viewModel;
+
+            messenger.Register<Shared.Messages.Common.EntityCreatedMessage<Circle>>(this, (r, m) => Close());
+            messenger.Register<Shared.Messages.Common.EntityUpdatedMessage<Circle>>(this, (r, m) => Close());
         }
 
         private async void Window_Loaded(object sender, RoutedEventArgs e)
