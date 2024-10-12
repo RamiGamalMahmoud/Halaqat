9913d6119a9c03f6c76d905c7e7923bee200af9e
Rami <rami.gamal.mahmoud@gmail.com>
2024-10-12 18:35:46 +0300

    add ServiceExtension.cs

---
+++ App/Halaqat.Features.MemorizingAndReview/ServiceExtension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Halaqat.Features.MemorizingAndReview
+{
+    public static class ServiceExtension
+    {
+        public static IServiceCollection ConfigureMemorizingAndReviewFeature(this IServiceCollection services)
+        {
+            services.AddMediatR((cfg) => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+            return services;
+        }
+    }
+}
