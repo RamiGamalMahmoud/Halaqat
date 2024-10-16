514ddc16447351b9f58e1cb8337db5e0fcdceed2
Rami <rami.gamal.mahmoud@gmail.com>
2024-10-16 22:09:26 +0300

    add CanShowMemorizingTable

--- App/Halaqat.Features.Students/Home/ViewModel.cs
+++ App/Halaqat.Features.Students/Home/ViewModel.cs
@@ -8,20 +8,36 @@ using System.Threading.Tasks;
 
 namespace Halaqat.Features.Students.Home
 {
-    internal partial class ViewModel(IMediator mediator, IMessenger messenger) : Shared.Common.HomeViewModelBase<Student>(mediator, messenger)
+    internal partial class ViewModel : Shared.Common.HomeViewModelBase<Student>
     {
+        public ViewModel(IMediator mediator, IMessenger messenger) : base(mediator, messenger)
+        {
+            User = _messenger.Send(new Shared.Messages.Users.LoggedInUserRequestMessage());
+        }
+
         public override async Task LoadDataAsync(bool isReload)
         {
             using (BusyWorkRunner.CreateBusyWork(DoBusyWork))
             {
+                Teacher = (await _mediator.Send(new Shared.Commands.Teachers.GetLoggedInTeacherCommand(User.Id))).Value;
                 Models = await _mediator.Send(new Shared.Commands.Common.GetAllCommand<Student>(isReload));
+                ShowStudentMemorizingAndReviewTableCommand.NotifyCanExecuteChanged();
             }
         }
 
-        [RelayCommand]
+        private bool CanShowMemorizingTable(Student student)
+        {
+            IsEnabled = Teacher is not null && student is not null && Teacher.Circles.Contains(student.Circle);
+            return Teacher is not null && student is not null && Teacher.Circles.Contains(student.Circle);
+        }
+
+        [ObservableProperty]
+        private bool _isEnabled;
+
+        [RelayCommand(CanExecute = nameof(CanShowMemorizingTable))]
         private async Task ShowStudentMemorizingAndReviewTable(Student student)
         {
-            await _mediator.Send(new Shared.Commands.MemorizingAndReviewCommands.ShowMemorizingAndReviewViewCommand(student));
+            await _mediator.Send(new Shared.Commands.MemorizingAndReviewCommands.ShowMemorizingAndReviewViewCommand(student, Teacher));
         }
 
         async partial void OnSearchTermChanged(string oldValue, string newValue)
@@ -36,6 +52,12 @@ namespace Halaqat.Features.Students.Home
 
         [ObservableProperty]
         private string _searchTerm;
+
+        public User User { get => _user; private set => SetProperty(ref _user, value); }
+        private User _user;
+
+        public Teacher Teacher { get => _teacher; private set => SetProperty(ref _teacher, value); }
+        private Teacher _teacher;
 
     }
 }
