2038b2c99b591b51ec117e1c1e2c06d2e783b836
Rami <rami.gamal.mahmoud@gmail.com>
2024-10-19 15:50:03 +0300

    add MemorizingAndReview Components

---
+++ App/Halaqat.Features.MemorizingAndReview/Components/ItemsComponent.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Halaqat.Features.MemorizingAndReview.Components
+{
+    public partial class ItemsComponent : UserControl
+    {
+        public ItemsComponent()
+        {
+            InitializeComponent();
+        }
+
+        public IEnumerable ItemsSource
+        {
+            get => (IEnumerable)GetValue(_itemsSourceProperty);
+            set => SetValue(_itemsSourceProperty, value);
+        }
+
+        private static DependencyProperty _itemsSourceProperty =
+            DependencyProperty.Register(nameof(ItemsSource), typeof(IEnumerable), typeof(ItemsComponent), new PropertyMetadata(null));
+    }
+}
---
+++ App/Halaqat.Features.MemorizingAndReview/Components/ProgramDayAppreciationComponent.xaml.cs
@@ -0,0 +1,64 @@
+﻿using Halaqat.Shared.Models;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Halaqat.Features.MemorizingAndReview.Components
+{
+    public partial class ProgramDayAppreciationComponent : UserControl
+    {
+        public ProgramDayAppreciationComponent()
+        {
+            InitializeComponent();
+        }
+
+        public bool CanSetAppreciation
+        {
+            get => (bool) GetValue(_canSetAppreciation);
+            set => SetValue(_canSetAppreciation, value);
+        }
+        private static DependencyProperty _canSetAppreciation = 
+            DependencyProperty.Register(nameof(CanSetAppreciation), typeof(bool), typeof(ProgramDayAppreciationComponent), new PropertyMetadata(false));
+
+        public bool HasItems
+        {
+            get => (bool) GetValue(_hasItems);
+            set => SetValue(_hasItems, value);
+        }
+        private static DependencyProperty _hasItems =
+            DependencyProperty.Register(nameof(HasItems), typeof(bool), typeof(ProgramDayAppreciationComponent), new PropertyMetadata(false));
+
+        public Appreciation Appreciation
+        {
+            get => (Appreciation) GetValue(_appreciation);
+            set => SetValue(_appreciation, value);
+        }
+        private static DependencyProperty _appreciation =
+            DependencyProperty.Register(nameof(Appreciation), typeof(Appreciation), typeof(ProgramDayAppreciationComponent), new PropertyMetadata(null));
+
+        public IEnumerable<Appreciation> Appreciations
+        {
+            get => (IEnumerable<Appreciation>) GetValue(_appreciations);
+            set => SetValue(_appreciations, value);
+        }
+        private static DependencyProperty _appreciations =
+            DependencyProperty.Register(nameof(Appreciations), typeof(IEnumerable<Appreciation>), typeof(ProgramDayAppreciationComponent), new PropertyMetadata(null));
+    
+        public ICommand Command
+        {
+            get => (ICommand) GetValue(_commandProperty);
+            set => SetValue(_commandProperty, value);
+        }
+        private static DependencyProperty _commandProperty =
+            DependencyProperty.Register(nameof(Command), typeof(ICommand), typeof(ProgramDayAppreciationComponent), new PropertyMetadata(null));
+
+        public object CommandParameter
+        {
+            get => GetValue(_commandParameter);
+            set => SetValue(_commandParameter, value);
+        }
+        private static DependencyProperty _commandParameter =
+            DependencyProperty.Register(nameof(CommandParameter), typeof(object), typeof(ProgramDayAppreciationComponent), new PropertyMetadata(null));
+    }
+}
