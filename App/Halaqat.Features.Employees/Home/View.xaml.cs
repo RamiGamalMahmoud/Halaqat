f3332ea7351fb9597167f59232405c3b42d919c1
Rami <rami.gamal.mahmoud@gmail.com>
2024-10-24 20:58:10 +0300

    register EntityCreateMessage

--- App/Halaqat.Features.Employees/Home/View.xaml.cs
+++ App/Halaqat.Features.Employees/Home/View.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Halaqat.Shared.Abstraction.Features.Employees;
+﻿using CommunityToolkit.Mvvm.Messaging;
+using Halaqat.Shared.Abstraction.Features.Employees;
+using Halaqat.Shared.Models;
 using System.Windows.Controls;
 
 namespace Halaqat.Features.Employees.Home
@@ -7,12 +9,17 @@ namespace Halaqat.Features.Employees.Home
     {
         private readonly ViewModel _viewModel;
 
-        public View(ViewModel viewModel)
+        public View(ViewModel viewModel, IMessenger messenger)
         {
             InitializeComponent();
             DataContext = viewModel;
 
             _viewModel = viewModel;
+
+            messenger.Register<Shared.Messages.Common.EntityCreatedMessage<Employee>>(this, async (r, m) =>
+            {
+                await Dispatcher.Invoke(async () => await _viewModel.LoadDataAsync(true));
+            });
         }
 
         private async void View_Loaded(object sender, System.Windows.RoutedEventArgs e)
