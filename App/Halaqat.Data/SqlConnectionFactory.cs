e287c136858d318a33ec5369ed37fdd8453669c4
Rami <rami.gamal.mahmoud@gmail.com>
2024-10-28 02:49:16 +0300

    add SqlConnectionFactory class

---
+++ App/Halaqat.Data/SqlConnectionFactory.cs
@@ -0,0 +1,13 @@
+﻿using Halaqat.Shared.Common;
+using Microsoft.Data.SqlClient;
+
+namespace Halaqat.Data
+{
+    public class SqlConnectionFactory(ConnectionStringFactory connectionStringFactory)
+    {
+        public SqlConnection CreateSqlConnection()
+        {
+            return new SqlConnection(connectionStringFactory.GetConnectionStringWithNotDatabase());
+        }
+    }
+}
