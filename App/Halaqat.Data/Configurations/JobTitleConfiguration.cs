067cb0ba056df9b2e30895da2668cb29b08096d8
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2025-01-24 16:33:08 +0200

    add more job titles

--- App/Halaqat.Data/Configurations/JobTitleConfiguration.cs
+++ App/Halaqat.Data/Configurations/JobTitleConfiguration.cs
@@ -20,7 +20,11 @@ namespace Halaqat.Data.Configurations
             builder.HasData(
             [
                 new { Id = 1, Name = "معلم", DateCreated = DateTime.Parse("2024-01-01") },
-                new { Id = 2, Name = "إداري", DateCreated = DateTime.Parse("2024-01-02") }
+                new { Id = 2, Name = "إداري", DateCreated = DateTime.Parse("2024-01-02") },
+                new { Id = 3, Name = "معلمة", DateCreated = DateTime.Parse("2024-01-01") },
+                new { Id = 4, Name = "إدارية", DateCreated = DateTime.Parse("2024-01-01") },
+                new { Id = 5, Name = "مشرف", DateCreated = DateTime.Parse("2024-01-01") },
+                new { Id = 6, Name = "مشرفة", DateCreated = DateTime.Parse("2024-01-01") },
             ]);
         }
     }
