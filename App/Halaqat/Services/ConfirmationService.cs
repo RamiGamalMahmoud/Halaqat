375e3f2e9437ce00ec60ac3104322a6265e7ac5c
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2025-01-27 00:33:43 +0200

    Create ConfirmationService.cs

---
+++ App/Halaqat/Services/ConfirmationService.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace Halaqat.Services
+{
+    internal class ConfirmationService
+    {
+        public bool Confirm(string message)
+        {
+            MessageBoxResult result = MessageBox.Show(message, "رسالة تأكيد", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            return result == MessageBoxResult.Yes;
+        }
+    }
+}
