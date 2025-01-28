bb52bbd601bd10263b109aefdf8256234edfbc86
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2025-01-28 14:38:11 +0200

    add ShowCircleDetailsCommand to Commands.Circles

--- App/Halaqat.Shared/Commands/Circles.cs
+++ App/Halaqat.Shared/Commands/Circles.cs
@@ -7,5 +7,6 @@ namespace Halaqat.Shared.Commands
     public static class Circles
     {
         public record SearchByName(string Name) : IRequest<IEnumerable<Circle>>;
+        public record ShowCircleDetailsCommand(int Id) : IRequest;
     }
 }
