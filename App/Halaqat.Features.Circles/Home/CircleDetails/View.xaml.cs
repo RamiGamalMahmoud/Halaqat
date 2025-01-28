7747eb45add7aa5c95497e8e5c04d5cc037a420d
RamiGamalMahmoud <43172711+RamiGamalMahmoud@users.noreply.github.com>
2025-01-28 15:11:41 +0200

    Create View.xaml.cs

---
+++ App/Halaqat.Features.Circles/Home/CircleDetails/View.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace Halaqat.Features.Circles.Home.CircleDetails
+{
+    internal partial class View : Window
+    {
+        public View(ViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+        }
+    }
+}
